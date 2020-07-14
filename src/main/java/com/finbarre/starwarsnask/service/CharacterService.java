86c48e121c7bc934b579c880ee87a586653d145d
Norbert Rostkowski <shadowrider@wp.pl>
2020-07-14 15:38:23 +0200

    refactoring services

--- src/main/java/com/finbarre/starwarsnask/service/CharacterService.java
+++ src/main/java/com/finbarre/starwarsnask/service/CharacterService.java
@@ -1,32 +1,9 @@
 package com.finbarre.starwarsnask.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-
 import com.finbarre.starwarsnask.helper.CharactersHelper;
-import com.finbarre.starwarsnask.payload.Character;
-import com.finbarre.starwarsnask.repository.CharacterRepository;
 
-@Service
-public class CharacterService {
+public interface CharacterService {
 
-	@Autowired
-	CharacterRepository characterRepository;
-
-	public CharactersHelper getAllCharacters(Integer pageNo, Integer pageSize, String sortBy) {
-
-		Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
-
-		Page<Character> pagedResult = characterRepository.findAll(paging);
-
-		CharactersHelper characterHelper = new CharactersHelper(pagedResult.getTotalElements(),
-				pagedResult.getTotalPages(), pagedResult.getContent());
-
-		return characterHelper;
-	}
+	CharactersHelper getAllCharacters(Integer pageNo, Integer pageSize, String sortBy);
 
 }
