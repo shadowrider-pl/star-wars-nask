ff884ff68e1f9ac3677c146e1af15f919277b058
Norbert Rostkowski <shadowrider@wp.pl>
2020-07-15 16:36:44 +0200

    empty

--- src/main/java/com/finbarre/starwarsnask/service/CharacterService.java
+++
@@ -1,9 +0,0 @@
-package com.finbarre.starwarsnask.service;
-
-import com.finbarre.starwarsnask.helper.CharactersHelper;
-
-public interface CharacterService {
-
-	CharactersHelper getAllCharacters(Integer pageNo, Integer pageSize, String sortBy);
-
-}
--- src/main/resources/homeworld-sorted.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO homeworld VALUES
-    (NULL, 'arid',10465,'1 standard','Tatooine',304,200000,23,1,'desert');
